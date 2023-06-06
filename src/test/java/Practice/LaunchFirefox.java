ba3d2b65c5f2959a5f0901fe5f8d82a9c31c5c75
vikra <vikra@Vicky>
2023-06-06 15:03:45 +0530

    commit

--- src/test/java/Practice/LaunchFirefox.java
+++ src/test/java/Practice/LaunchFirefox.java
@@ -1,9 +1,17 @@
 package Practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LaunchFirefox {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.youtube.com/");
+		
 
 	}
 
