607a85590e779134af8b201704af35230ac7bca3
vikra <vikra@Vicky>
2023-06-07 06:38:51 +0530

    commit

--- src/test/java/Practice/LaunchFirefox.java
+++ src/test/java/Practice/LaunchFirefox.java
@@ -1,6 +1,7 @@
 package Practice;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
